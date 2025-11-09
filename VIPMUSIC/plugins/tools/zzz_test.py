08c609a08be845031c679fc3c00ba208a64ffbdf
GhosttBatt <ghosttbatt@gmail.com>
2025-11-09 20:58:29 +0530

    Delete VIPMUSIC/plugins/tools/zzz_test.py

--- VIPMUSIC/plugins/tools/zzz_test.py
+++
@@ -1,9 +0,0 @@
-from VIPMUSIC import app
-from pyrogram import filters
-
-print("[ZZZ-Test] Plugin loaded!")
-
-@app.on_message(filters.command("zzztest") & filters.group)
-async def zzz(_, message):
-    print("[ZZZ-Test] /zzztest command triggered!")
-    await message.reply_text("✅ zzztest works!")
