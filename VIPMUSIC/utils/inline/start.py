df74407c8d11e98888ce0b060112234241d2bb75
GhosttBatt <ghosttbatt@gmail.com>
2025-11-10 16:41:25 +0530

    start.py

--- VIPMUSIC/utils/inline/start.py
+++ VIPMUSIC/utils/inline/start.py
@@ -6,15 +6,11 @@ from VIPMUSIC import app
 def start_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
+            InlineKeyboardButton(text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"),
         ],
         [
             InlineKeyboardButton(text="💕  𝐇𖾔𖾘𖽳 🦋", callback_data="settings_back_helper"),
-            InlineKeyboardButton(
-                text="💕 𝐒𖾔𖾓 🦋", callback_data="settings_helper"
-            ),
+            InlineKeyboardButton(text="💕 𝐒𖾔𖾓 🦋", callback_data="settings_helper"),
         ],
         [
             InlineKeyboardButton(text="💕 𝐆𖽷𖽙𖽪𖽳 🦋", url=config.SUPPORT_CHAT),
@@ -26,10 +22,7 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text="💕 𝐊𖽹𖽴𖽡꘍𖽳 𝐌𖽞  🦋",
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
+            InlineKeyboardButton(text="💕 𝐊𖽹𖽴𖽡꘍𖽳 𝐌𖽞  🦋",url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
             InlineKeyboardButton(text="💕 𝐆𖽷𖽙𖽪𖽳 🦋", url=config.SUPPORT_CHAT),
